190db8c0b8854ff2f23495636aa9d3f759d9cc32
lindembergs <lindembergparaiba@gmail.com>
2023-04-24 01:01:49 -0300

    primeiros passos angular

---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent {
+
+  title = "Test";
+
+  value = 'aaa';
+  onEnter(value: string) { this.value = value; }
+
+
+
+ 
+  
+    
+  
+
+}
